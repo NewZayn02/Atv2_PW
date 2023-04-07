75b5240bce378fca8ca47436d1f022c42f95af53
NewZayn02 <118941322+NewZayn02@users.noreply.github.com>
2023-04-06 23:20:29 -0300

    3 files modified

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 const inputItem = document.querySelector("#inputItem");
 const inputQuantidade = document.querySelector("#inputQuantidade");
 const inputPreco = document.querySelector("#inputPreco");
